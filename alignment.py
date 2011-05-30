f4e277beebbdbf96c2b9b7fc9e84dd52a4309626
Tomasz Pazurkiewicz <t.pazurkiewicz@gmail.com>
2011-05-30 23:31:09 +0200

    Fix bio.align.applications -> clustalw

--- alignment.py
+++ alignment.py
@@ -1,6 +1,7 @@
 from Bio.Align.Applications import ClustalwCommandline
 
 import inout
+import os.path
 
 COMPULSORY_SETTINGS = ('CLUSTALW_PATH',)
 
@@ -14,9 +15,17 @@ class Alignment(object):
     def run(self):
         from settings import get_setting
         exe = get_setting('CLUSTALW_PATH')
-        cline = ClustalwCommandline(exe, infile=self.input, outfile='temp_file.txt')
-        stdout, stderr = cline()
-
+        tree_file = 'tree.txt'
+        cline = ClustalwCommandline(exe, infile=self.input, newtree=tree_file)
+        cline()
+        cline = ClustalwCommandline(exe, infile=self.input, outfile='out.txt')
+        ###Fix "p.checker_function = lambda x: os.path.exists"
+        param = [i for i,p in enumerate(cline.parameters) 
+            if 'usetree' in p.names]
+        cline.parameters[param[0]].checker_function = os.path.exists
+        ###
+        setattr(cline, 'usetree', tree_file)
+        cline()
 
 def run(*args, **kwargs):
     return Alignment(*args, **kwargs).run()
